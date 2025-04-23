d9b264a858c7fb05cf06a95be760e32ff58bfc97
CalebMostyn <112972460+CalebMostyn@users.noreply.github.com>
2025-04-23 17:24:45 -0400

    Initial Commit

---
+++ final-project/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * final-project.cpp
+ *
+ * Created: 4/23/2025 5:14:15 PM
+ * Author : Caleb
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
